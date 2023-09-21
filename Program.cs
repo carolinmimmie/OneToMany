b07ddd34f433a5aa30ac7f95ce95e9609704497b
carolinmimmie <carolinmimmie@gmail.com>
2023-09-21 09:06:28 +0200

    add information in files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using static System.Console;
+
+namespace OneToMany;
+
+class Program
+{
+    public static void Main()
+    {
+        Title = "OneToMany"; //sätter namnet på tabben
+        CursorVisible = false; //stänger av markör
+
+        while (true) //Loop som körs tills vi stänger ner den
+        {
+            WriteLine("1. Lista produkter");
+
+            var keyPressed = ReadKey(true); //hämtar in värdet
+
+            Clear();
+
+
+            switch (keyPressed.Key)
+            {
+                case ConsoleKey.D1: //case för menyval1
+                case ConsoleKey.NumPad1:
+
+
+
+                    break;
+
+            }
+            Clear();
+
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# OneToMany
+
+Enkel konsol-baserad applikaton för hantering av projekt.
+
+Syftet att öva på one to many.
+
+## Kom igång
+
+1. Klona repot.
+
+2. Sätt upp databasen.
+
+`dotnet ef database update`
+
+3. Starta applikationen.
